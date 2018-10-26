5e15d4dca8ca5155fc562ffdd6cda278d00c9a28
lichunzhu <lichunzhu@stu.xjtu.edu.cn>
2018-10-26 22:02:09 +0800

    10.26 add ToutiaoUtil

--- src/main/java/com/baine/toutiao/service/NewsService.java
+++ src/main/java/com/baine/toutiao/service/NewsService.java
@@ -2,10 +2,17 @@ package com.baine.toutiao.service;
 
 import com.baine.toutiao.dao.NewsDAO;
 import com.baine.toutiao.model.News;
+import com.baine.toutiao.util.ToutiaoUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class NewsService {
@@ -18,5 +25,23 @@ public class NewsService {
 
     public List<News> getLatestNews(int userId, int offset, int limit) {
         return newsDAO.selectByUserIdAndOffset(userId, offset, limit);
+    }
+
+    public String saveImage(MultipartFile file) throws IOException {
+        // xxx._=someName.jpg
+        int dotPos = file.getOriginalFilename().lastIndexOf(".");
+        if (dotPos < 0) {
+            return null;
+        }
+        String fileExt = file.getOriginalFilename().substring(dotPos + 1).toLowerCase();
+        if (!ToutiaoUtil.isFileAllowed(fileExt)) {
+            return null;
+        }
+
+        String fileName = UUID.randomUUID().toString().replaceAll("-", "") + "." + fileExt;
+        Files.copy(file.getInputStream(), new File(ToutiaoUtil.IMAGE_DIR + fileName).toPath(),
+                   StandardCopyOption.REPLACE_EXISTING);
+        // xxx.???
+        return ToutiaoUtil.TOUTIAO_DOMAIN + "image?name=" + fileName;
     }
 }
--- src/main/java/com/baine/toutiao/util/ToutiaoUtil.java
+++ src/main/java/com/baine/toutiao/util/ToutiaoUtil.java
@@ -10,6 +10,18 @@ import java.util.Map;
 public class ToutiaoUtil {
     private static final Logger logger = LoggerFactory.getLogger(ToutiaoUtil.class);
 
+    public static String TOUTIAO_DOMAIN = "http://127.0.0.1:8080/";
+    public static String IMAGE_DIR = "F:/toutiao_upload/";
+    public static String[] IMAGE_FILE_EXT = new String[] {"png", "bmp", "jpg", "jpeg"};
+
+    public static boolean isFileAllowed(String fileEXT) {
+        for (String ext: IMAGE_FILE_EXT) {
+            if(ext.equals(fileEXT))
+                return true;
+        }
+        return false;
+    }
+
     public static String getJSONString(int code) {
         JSONObject json = new JSONObject();
         json.put("code", code);
